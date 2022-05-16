1c8167079d621989e232e066025e2f01036badee
Luca Venerando Greco <lucag2001@gmail.com>
2022-05-16 22:24:49 +0200

    feat: added basic classes

---
+++ Coords.java
@@ -0,0 +1,17 @@
+public class Coords {
+    private Integer x;
+    private Integer y;
+
+    Coords(Integer x, Integer y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getX () {
+        return this.x;
+    }
+
+    public Integer getY () {
+        return this.y;
+    }
+}
