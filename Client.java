b264d9bbdfe02f29dcea62d8c7b9881804b7d469
Riccardo Cuccia <riccardo.cuccia02@gmail.com>
2022-05-16 23:31:47 +0200

    feat: read physical_composition yaml file

--- Client.java
+++ Client.java
@@ -1,6 +1,6 @@
 public class Client {
     public static void main(String args[]) {
-
+        Crossword c = new Crossword("orizzontali.txt", "verticali.txt");
     }
     
 }
--- Crossword.java
+++ Crossword.java
@@ -1,5 +1,9 @@
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
 
 public class Crossword extends Subject{
     public List<String> orizzontali = new ArrayList<>();
@@ -8,9 +12,16 @@ public class Crossword extends Subject{
     public Integer height;
     public List<Coords> blacks = new ArrayList<>();
     public List<Number> numbers = new ArrayList<>();
+    public Yaml yaml = new Yaml();
+    public String document = "physical_composition.yaml";
 
     Crossword(String orizzontali_filename, String verticali_filename){
         orizzontali = Utils.getSuggestions(orizzontali_filename);
         verticali = Utils.getSuggestions(verticali_filename);
+        InputStream inputStream = this.getClass()
+            .getClassLoader()
+            .getResourceAsStream(document);
+        Map<String, Object> obj = yaml.load(inputStream);
+        System.out.println(obj);
     }
 }
