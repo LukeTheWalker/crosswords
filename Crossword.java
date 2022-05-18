340d213251403d56228f10b369ca9421002d515c
Riccardo Cuccia <riccardo.cuccia02@gmail.com>
2022-05-18 12:14:22 +0200

    chore: word input uses stream

--- Crossword.java
+++ Crossword.java
@@ -1,6 +1,8 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PrimitiveIterator;
+import java.util.stream.IntStream;
 
 public class Crossword extends Subject{
     private List<String> orizzontali = new ArrayList<>();
@@ -32,25 +34,54 @@ public class Crossword extends Subject{
     private boolean insertWord(Coords coords, String direction, String word){
         int x = coords.getX_cord();
         int y = coords.getY_cord();
-        if(direction.equals("o")){
-            if(x+word.length() > this.physicalComposition.getSize().getWidth()) return false;
-        }else{
-            if(y+word.length() > this.physicalComposition.getSize().getHeight()) return false;
-        }
-        for(int i = 0; i < word.length(); i++){
-            if(direction.equals("o")){
-                if(grid[x+i][y] == "█") return false;
-            }else{
-                if(grid[x][y+i] == "█") return false;
+        IntStream stream = IntStream.rangeClosed(0, word.length()-1);
+        IntStream stream2 = IntStream.rangeClosed(0, word.length()-1);
+        PrimitiveIterator.OfInt answer = stream.iterator();
+        if(direction == "o"){
+            if(x+word.length() <= this.physicalComposition.getSize().getWidth()){
+                while(answer.hasNext()){
+                    if(grid[x+answer.nextInt()][y] == "█") return false;
+                }
             }
-        }
-        for(int i = 0; i < word.length(); i++){
-            insertLetter(x, y, word.charAt(i));
-            if(direction.equals("o")) x++;
-            else y++;
+            stream2.forEach(i -> {
+                insertLetter(x+i, y, word.charAt(i));
+            });
+        }else{
+            if(y+word.length() <= this.physicalComposition.getSize().getHeight()){
+               while(answer.hasNext()){
+                if(grid[x][y+answer.nextInt()] == "█") return false;
+               }
+            }
+            stream2.forEach(i -> {
+                insertLetter(x, y+i, word.charAt(i));
+            });
         }
         return true;
     }
+
+
+    // private boolean insertWord(Coords coords, String direction, String word){
+    //     int x = coords.getX_cord();
+    //     int y = coords.getY_cord();
+    //     if(direction.equals("o")){
+    //         if(x+word.length() > this.physicalComposition.getSize().getWidth()) return false;
+    //     }else{
+    //         if(y+word.length() > this.physicalComposition.getSize().getHeight()) return false;
+    //     }
+    //     for(int i = 0; i < word.length(); i++){
+    //         if(direction.equals("o")){
+    //             if(grid[x+i][y] == "█") return false;
+    //         }else{
+    //             if(grid[x][y+i] == "█") return false;
+    //         }
+    //     }
+    //     for(int i = 0; i < word.length(); i++){
+    //         insertLetter(x, y, word.charAt(i));
+    //         if(direction.equals("o")) x++;
+    //         else y++;
+    //     }
+    //     return true;
+    // }
 
     public void updateGrid(Coords coords, String direction, String word){
         if(!insertWord(coords, direction, word)){
--- TerminalCursor.java
+++ TerminalCursor.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.CountDownLatch;
+
 public class TerminalCursor {
     private static final String ESC = "\033[";
     public static void clearLines (Integer n){
@@ -9,6 +11,11 @@ public class TerminalCursor {
 
         cursorUp(n);
     }
+
+    public static void setCursorPosition(int row, int column){
+        System.out.print(ESC+row+";"+column+"f");
+    }
+
     public static void cursorUp(Integer n){
         System.out.print("\r");
         System.out.print(ESC + Integer.toString(n) + "A");
