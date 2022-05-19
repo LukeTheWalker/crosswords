cb7f428c28b284b191da152491f6c0b7d9e87f60
Riccardo Cuccia <riccardocuccia@gmail.com>
2022-05-19 16:50:50 +0200

    misc: test comment

--- Client.java
+++ Client.java
@@ -4,5 +4,6 @@ public class Client {
     public static void main(String args[]) throws IOException {
         MenuContext menu = new MenuContext("orizzontali.txt", "verticali.txt", "physical_composition.yaml");
         menu.start();
+        //comment
     }  
 }
