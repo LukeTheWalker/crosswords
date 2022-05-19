445f8b888aa8486ac94a909af187bc95324b3f0c
Riccardo Cuccia <riccardocuccia@gmail.com>
2022-05-19 17:07:22 +0200

    misc: removed test comment

--- Client.java
+++ Client.java
@@ -4,6 +4,5 @@ public class Client {
     public static void main(String args[]) throws IOException {
         MenuContext menu = new MenuContext("orizzontali.txt", "verticali.txt", "physical_composition.yaml");
         menu.start();
-        //comment
     }  
 }
